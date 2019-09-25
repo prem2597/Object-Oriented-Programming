9ba9e93e21ebebd65525a11c31e535bd216486dd
prem2597 <prembammidi25@msitprogram.net>
2019-09-25 18:55:09 +0530

    Updated

---
+++ Day3/Assignment3/Squares.java
@@ -0,0 +1,33 @@
+/**
+ * Given an array of Integers, return an array of the square
+ * of each values in the array. 
+ * 
+ * Input : {1, 2, 3}
+ * Output : {1, 4, 9}
+ * 
+ * @author Siva Sankar
+ * @author Mayank
+ */
+import java.lang.Math;
+public class Squares {
+
+    /**
+     * This method returns the array of ints that are squares of each element
+     * in the input array.
+     * 
+     * @param nums, the input array.
+     * 
+     * @return the array of ints that are squares of each element
+     * in the input array.
+     * 
+     */
+    public static int[] squared(int[] nums) {
+        //  Your code goes here.
+        for (int i=0; i<nums.length;i++)
+        {
+            nums[i]= (int) Math.pow(nums[i],2);
+        }
+        
+        return nums;
+    }
+}
