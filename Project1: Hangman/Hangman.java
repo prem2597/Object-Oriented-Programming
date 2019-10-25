6a52192e6bf18536d1db45d90c7f4a3cf4f7459c
prem2597 <prembammidi25@msitprogram.net>
2019-10-25 14:41:14 +0530

    finalsub2

--- Project1: Hangman/Hangman.java
+++ Project1: Hangman/Hangman.java
@@ -411,7 +411,7 @@ class Hangman {
                 + ":  PlayerScore " + players[i].getPlayerScore());
         }
         System.out.println("Game Over !");
-        System.out.println("Congratulations" + players[0].getPlayerName());
+        System.out.println("Congratulations  " + "''" + players[0].getPlayerName() + "''");
         System.out.println("👍👍👍");
     }
 }
