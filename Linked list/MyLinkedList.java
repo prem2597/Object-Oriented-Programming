1378eeea2eff526b7a8d8b41759a9b7bb459173f
prem2597 <prembammidi25@msitprogram.net>
2019-10-31 10:23:51 +0530

    git commit

--- Linked list/MyLinkedList.java
+++ Linked list/MyLinkedList.java
@@ -2,27 +2,45 @@
  * List of my linkedlists.
  */
 class MyLinkedList {
+
     /**
      * Constructs an empty list with size initialized to zero.
      */
-    int list = 0;
-    public MyLinkedList() {}
+    // int list = 0;
+    private Node head;
+    public MyLinkedList() {
+        head = null;
+    }
     /**
      * Appends the specified element to the end of this list.
      *
      * @param      data  The data
      */
-    public void add(String data){}
+    public void add(String data){
+        if (head == null) {
+            addFirst(data);
+        } else {
+            Node t = head;
+            while (t.next != null) {
+                t = t.next;
+            }
+            t.next = new Node(data, null);
+        }
+    }
     /**
      * Inserts the specified element at the beginning of this list.
      *
      * @param      data  The data
      */
-    public void addFirst(String data){}
+    public void addFirst(String data){
+        head = new Node(data, head);
+    }
     /**
      * Removes all of the elements from this list.
      */
-    public void clear() {}
+    public void clear() {
+        head = null;
+    }
     /**
      * Returns true if this list contains the specified element.
      *
@@ -30,7 +48,19 @@ class MyLinkedList {
      *
      * @return     { description_of_the_return_value }
      */
-    public boolean contains(String data) { return false; }
+    public boolean contains(String data) {
+        if(head == null) {
+            return false;
+        }
+        Node t = head;
+        while (t.next != null) {
+            t = t.next;
+            if(t.data.equals(data)) {
+                return true;
+            }
+        }
+        return false;
+    }
     /**
      * Returns the element at the specified position in this list.
      *
@@ -38,41 +68,111 @@ class MyLinkedList {
      *
      * @return     { description_of_the_return_value }
      */
-    public String get(int index){ return null; }
+    public String get(int index){
+        if(head == null) {
+            return null;
+        }
+        Node t = head;
+        for (int i = 0; i< index; i++) {
+            t = t.next;
+        }
+        if(t == null) {
+            return null;
+        }
+        return t.data;
+    }
     /**
      * Returns the first element in this list.
      *
      * @return     The first.
      */
-    public String getFirst() { return null; }
+    public String getFirst() {
+        if (head == null) {
+            return null;
+        }
+        return head.data;
+    }
     /**
      * Returns the last element in this list.
      *
      * @return     The last.
      */
-    public String getLast() { return null; }
+    public String getLast() {
+        if (head == null) {
+            return null;
+        }
+        Node t = head;
+        while ( t.next != null) {
+            t = t.next;
+        }
+        return t.data;
+    }
     /**
      * Retrieves and removes the head (first element) of this list.
      *
      * @return     { description_of_the_return_value }
      */
-    public String remove() { return null; }
+    public String remove() {
+        if(head == null) {
+            return null;
+        }
+        String t = getFirst();
+        head = head.next;
+        return t;
+    }
     /**
      * Removes and returns the last element from this list.
      *
      * @return     { description_of_the_return_value }
      */
-    public String removeLast() { return null; }
+    public String removeLast() {
+        if (head == null) {
+            return null;
+        }
+        if (head.next == null) {
+            String value = head.data;
+            head = null;
+            return value;
+        }
+        Node t = head;
+        while (t.next.next != null) {
+            t = t.next;
+        }
+        String lastData = t.next.data;
+        t.next = null;
+        return lastData;
+    }
     /**
      * Returns the number of elements in this list.
      *
      * @return     { description_of_the_return_value }
      */
-    public int size() { return 0; }
+    public int size() {
+        if (head == null) {
+            return 0;
+        }
+        Node t = head;
+        int count = 0;
+        while (t != null) {
+            count++;
+            t = t.next;
+        }
+        return count;
+    }
     /**
      * Returns a string representation of the object.
      *
      * @return     String representation of the object.
      */
-    public String toString() { return ""; }
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        Node t = head;
+        if (t == null) {
+            return "LinkedList is empty";
+        }
+        while (t != null) {
+            sb.append("[" + t.data + "]");
+            t = t.next;
+        }
+        return sb.toString();}
 }
--- Linked list/Node.java
+++ Linked list/Node.java
@@ -1,22 +1,13 @@
-class LinkedList {
-	/**
-	 * class for Node
-	 */
-	class Node {
-		//Todo
-		int data;
-		Node next;
-		Node (int d) {
-			data = d;
-			next = null;
-		}
-	}
-	public static void main(String[] args) {
-		LinkedList llist = new LinkedList();
-		llist.head = new Node(1);
-		llist.second = new Node(2);
-		llist.third = new Node(3);
-		llist.head.next = second;
-		second.next = third;
+/**
+ * class for Node
+ */
+class Node {
+	//Todo
+	String data;
+	Node next;
+	Node (String d, Node next) {
+		this.data = d;
+		this.next = next;
 	}
 }
+	
